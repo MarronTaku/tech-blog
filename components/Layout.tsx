e33ec5608ea76a07ee8ed2e427aa69646f43b7b5
MarronTakue <marron_taku@icloud.com>
2024-12-01 03:08:08 +0000

    設定ファイルを追加しソースコードを修正しました。

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,17 +1,21 @@
 import Link from "next/link";
-import React from "react";
+import React, { FC, ReactNode } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
- 
-const Layout = () => {
+import { LayoutProps } from "@/types/types";
+
+
+const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <div className="relative overflow-hidden">
       <div className="flex flex-col items-center max-w-2xl w-full mx-auto">
         {/* nav部分をコーポネント化 */}
         <Navbar />
-        <main></main>
+        {/*  */}
+        <main className="w-full pd-12 px-4">{children}</main>
         {/* footer部分をコーポネント化 */}
         <Footer />
+        {/*  */}
       </div>
     </div>
   );
---
+++ site.config.ts
@@ -0,0 +1,9 @@
+import { title } from "process";
+
+// サイト共通部分の設定
+export const siteConfig = {
+  title: "Notion Next Blog",
+  twitterUrl: "",
+  instagramUrl: "https://www.instagram.com/marron.taku/",
+  githubUrl: "https://github.com/MarronTaku",
+}
